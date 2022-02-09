1c3f2c1104804724015440476f18f160408d49af
Diego Salas <diesalasmart@gmail.com>
2022-02-08 21:37:03 -0500

    remove unused code

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react'
-import { Box, Typography, Container } from '@mui/material'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
 
 export default class Layout extends Component {
   render() {
-    // console.log(this.props.context.history);
-    // console.log(this.props.navigation);
     return (
       <>
         <Header {...this.props}/>
         <Outlet/>
-        <div>Footer</div>
       </>
-      // <Container maxWidth='lg'>
-      //   <Box sx={{my: 4}}>
-      //     <Typography variant='h2' component='h2' gutterBottom>
-      //       Hola mundo con Material UI
-      //     </Typography>
-      //   </Box>
-      // </Container>
     )
   }
 }
