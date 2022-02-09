38eb37eb141a1df3b094388507edebf77c0aa09d
Diego Salas <diesalasmart@gmail.com>
2022-02-08 21:10:03 -0500

    handle no route match

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const Layout = React.lazy(() => import('./layout/Layout'))
+const Content = React.lazy(() => import('./layout/Content'))
+const A = React.lazy(() => import('./layout/A'))
+const B = React.lazy(() => import('./layout/B'))
 
 const loading = <div>Loading</div>
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <React.Suspense fallback={loading}>
           <Routes>
-            <Route path="/" name="Home" element={<Layout {...this.props}/>}/>
+            <Route path='/' element={<Layout {...this.props}/>}>
+              <Route path='home' element={<Content {...this.props} element={A}/>}/>
+              <Route path='posts' element={<Content {...this.props} element={B}/>}/>
+              <Route path='*' element={<div>Not found</div>}/>
+            </Route>
           </Routes>
         </React.Suspense>
       </BrowserRouter>
     )
   }
 }
-
-export default App
