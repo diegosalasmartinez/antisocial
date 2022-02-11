9145c34d0f29611240e99d7ca66075569847203c
Diego Salas <diesalasmart@gmail.com>
2022-02-10 23:18:46 -0500

    display icons on sidebar

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,5 +1,5 @@
-import { Box, Button, Stack, Typography } from '@mui/material'
 import React, { Component } from 'react'
+import { Box, Button, Grid, Stack, Typography } from '@mui/material'
 import navOptions from './options/navOptions'
 
 export default class Sidebar extends Component {
@@ -8,15 +8,28 @@ export default class Sidebar extends Component {
     if (path) this.props.navigate(path)
   }
 
+  getVariantText = (h) => {
+    const { pathname } = this.props.location; 
+    const { path } = h; 
+    return pathname === path ? 'contained' : 'text';
+  }
+
   render() {
     return (
       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-        <Stack spacing={2}>
+        <Stack spacing={2} sx={{width: '100%'}}>
           { navOptions.map((h, index) => 
-            <Button key={index} sx={{ my: 0.2, color: 'white', display: 'block' }} variant="text">
-              <Typography textAlign="left" variant='subtitle1' sx={{textTransform: 'capitalize'}} onClick={e => this.selectOption(e, h.path)}>
-                {h.name}
-              </Typography>
+            <Button key={index} sx={{ my: 0.2, color: 'white', display: 'block' }} variant={this.getVariantText(h)} onClick={e => this.selectOption(e, h.path)}>
+              <Grid container spacing={1}>
+                <Grid item xs={4}>
+                  {h.icon}
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography textAlign="left" variant='subtitle1' sx={{textTransform: 'capitalize'}}>
+                    {h.name}
+                  </Typography>
+                </Grid>
+              </Grid>
             </Button>
           ) }
         </Stack>
--- src/layout/options/navOptions.js
+++ src/layout/options/navOptions.js
@@ -1,11 +1,15 @@
+import { Home, Newspaper } from '@mui/icons-material'
+
 const navOptions = [
   {
     name: 'Home',
-    path: '/'
+    path: '/',
+    icon: <Home/>
   },
   {
     name: 'My Posts',
-    path: '/user'
+    path: '/user',
+    icon: <Newspaper/>
   },
 ]
 
