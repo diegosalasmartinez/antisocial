4ecadd92cf9cafbf2f1c70dad269080b92b62e5e
Diego Salas <diesalasmart@gmail.com>
2022-02-11 00:51:30 -0500

    create testing components

--- src/pages/A.js
+++ src/pages/A.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
+import Wrapper from '../components/Wrapper';
 
 export default class A extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() { 
+    setTimeout(() => {  this.setState({loading: false}); }, 10000);
+  }
+
   render() {
-    return <div>A
-    </div>;
+    const { loading } = this.state
+
+    return (
+      <Wrapper loading={loading}>
+        <div>
+          SOY EL COMPONENTE A
+        </div>
+      </Wrapper>
+    )
   }
 }
