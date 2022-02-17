ce2fdc9bfdd0d9cafafeab0c82e8790386b76eb7
Diego Salas <diesalasmart@gmail.com>
2022-02-17 17:14:13 -0500

    modify Page component to show notifications on screen

--- src/layout/Page.js
+++ src/layout/Page.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import MyNotification from '../components/MyNotification'
 
 export default function Page(props) {
   const location = useLocation();
   const navigate = useNavigate();
+  const [error, setError] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const showError = (msg) => {
+    setError(true);
+    setErrorMessage(msg);
+  }
+  
+  const closeNotification = () => {
+    setError(false);
+    setErrorMessage('');
+  }
 
   return (
-    <props.element location={location} navigate={navigate}/>
+    <>
+      <MyNotification open={error} message={errorMessage} severity='error' closeNotification={closeNotification}/>
+      <props.element location={location} navigate={navigate} showError={showError}/>
+    </>
   )
 }
