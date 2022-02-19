50a3a029a93609bd3ac4c791a607bd8011ed94c1
Diego Salas <diesalasmart@gmail.com>
2022-02-18 23:35:27 -0500

    create post form component

--- src/pages/Posts/CreatePost.js
+++ src/pages/Posts/CreatePost.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react'
 import { Box, Typography } from '@mui/material'
 import MyButton from '../../components/MyButton'
+import MyTextField from '../../components/MyTextField'
+import PostModel from '../../services/models/PostModel';
 
 export default class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: new PostModel()
+    }
+  }
+
+  onChange = (key, isNumeric = false) => (e = {}) => {
+    const { post } = this.state;
+    let val = isNumeric ? parseInt(e.target.value || '0') : e.target.value;
+    let objectUpdated = { ...post };
+
+    const keys = key.split(".");
+    if (keys.length > 1) {
+      objectUpdated[keys[0]][keys[1]] = val;
+    } else {
+      objectUpdated[key] = val;
+    }
+    this.setState({post: objectUpdated});
+  }
+
   render() {
+    const { post } = this.state;
+    const { title, body } = post;
+
     return (
       <Box>
         <Typography textAlign="left" variant='h6'>
           Post your idea
         </Typography>
-        <Typography textAlign="left" variant='body1'>
-          Create your post
-        </Typography>
+        <Box className='form' sx={{mt: '1.5rem'}}>
+          <MyTextField param='title' label='Title' value={title} onChange={this.onChange}/>
+          <MyTextField param='body' label='Body' value={body} mb={0} onChange={this.onChange}/>
+        </Box>
         <Box className='jc-r'>
           <MyButton text='Cancel' variant='secondary' onClick={this.props.onCancel}/>
+          <MyButton text='Post' onClick={() => this.props.onPost(post)}/>
         </Box>
       </Box>
     )
---
+++ src/services/models/PostModel.js
@@ -0,0 +1,37 @@
+export default class PostModel {
+  _id = "";
+  title = "";
+  body = "";
+  author = "";
+  date = new Date();
+}
+
+const validate = (user) => {
+    let errors = {
+        DNI: null,
+        name: null,
+        lastName: null,
+        email: null,
+        phone: null,
+        username: null,
+        password: null
+    }
+    if (!user.personInfo.DNI) {
+        errors.DNI = "DNI is mandatory";
+    }
+    if (!user.personInfo.name) {
+        errors.name = "Name is mandatory";
+    }
+    if (!user.personInfo.lastName) {
+        errors.lastName = "Last name is mandatory";
+    }
+    if (!user.personInfo.email) {
+        errors.email = "Email is mandatory";
+    }
+    if (!user.personInfo.phone) {
+        errors.phone = "Phone is mandatory";
+    }
+    return errors;
+}
+
+export { validate }
