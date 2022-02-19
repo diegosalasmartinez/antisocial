3094a74beedb21e97d77a79b1d77034ea2a33fa9
Diego Salas <diesalasmart@gmail.com>
2022-02-18 19:05:34 -0500

    refactor to functional component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Box } from '@mui/material'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
-import { Box } from '@mui/material'
 import colors from '../theme/colors'
 
-export default class Layout extends Component {
-  render() {
-    return (
-      <Box sx={{backgroundColor: colors.PRIMARY, height: '100vh'}}>
-        <Header {...this.props}/>
-        <Outlet/>
-      </Box>
-    )
-  }
+export default function Layout(props) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const auth = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (!auth.token){
+      navigate("/sign-in")   
+    }
+  },[auth.token]);
+
+  return (
+    <Box sx={{backgroundColor: colors.PRIMARY, height: '100vh'}}>
+      <Header {...props}/>
+      <Outlet/>
+    </Box>
+  )
 }
