39c1475a8cec2675e2c82424fe267266be2aacd1
Diego Salas <diesalasmart@gmail.com>
2022-02-18 23:35:07 -0500

    update component styles

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -6,8 +6,10 @@ export default class MyButton extends Component {
   render() {
     const { text = '', my = 2, variant = 'primary' } = this.props
     const bgColor = variant === 'primary' ? colors.BLUE : colors.SECONDARY;
+    const mr = variant === 'primary' ? '0' : '10px'
+    
     return (
-      <Button sx={{ my: my, color: colors.WHITE, display: 'block', backgroundColor: bgColor, '&:hover': {backgroundColor: bgColor, filter: 'brightness(1.75)'}}} variant='contained' onClick={this.props.onClick}>
+      <Button sx={{ mr: mr, my: my, color: colors.WHITE, display: 'block', backgroundColor: bgColor, '&:hover': {backgroundColor: bgColor, filter: 'brightness(1.75)'}}} variant='contained' onClick={this.props.onClick}>
         <Typography variant='body1' noWrap component="div" textTransform='none'>
           {text}
         </Typography>
--- src/components/MyTextField.js
+++ src/components/MyTextField.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 
 export default class MyTextField extends Component {
   render() {
-    const { param = '', label = '', value = '', type = 'text' } = this.props;
+    const { param = '', label = '', value = '', type = 'text', mb = '1.25rem' } = this.props;
 
     return (
-      <TextField id={param} label={label} value={value} fullWidth variant='outlined' type={type} onChange={this.props.onChange(param)} autoComplete='new-password'/>
+      <TextField id={param} label={label} value={value} fullWidth variant='outlined' type={type} sx={{mb: mb}} onChange={this.props.onChange(param)} autoComplete='new-password'/>
     )
   }
 }
