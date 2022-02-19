7aa40042a0a33b2b430335b4089e9f5761de215c
Diego Salas <diesalasmart@gmail.com>
2022-02-19 17:45:30 -0500

    show progress on MyButton when action is loading

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
-import { Button, Typography } from '@mui/material'
+import { Button, CircularProgress, Typography } from '@mui/material'
 import colors from '../theme/colors'
 
 export default class MyButton extends Component {
   render() {
-    const { text = '', my = 2, variant = 'primary' } = this.props
+    const { text = '', my = 2, variant = 'primary', loading } = this.props
     const bgColor = variant === 'primary' ? colors.BLUE : colors.SECONDARY;
     const mr = variant === 'primary' ? '0' : '10px'
     
     return (
       <Button sx={{ mr: mr, my: my, color: colors.WHITE, display: 'block', backgroundColor: bgColor, '&:hover': {backgroundColor: bgColor, filter: 'brightness(1.75)'}}} variant='contained' onClick={this.props.onClick}>
-        <Typography variant='body1' noWrap component="div" textTransform='none'>
+        <Typography variant='body1' noWrap component="div" textTransform='none' sx={{display: 'flex', alignItems: 'center'}}>
+          { loading && <CircularProgress size={20} sx={{mr: '10px'}}/> }
           {text}
         </Typography>
       </Button>
