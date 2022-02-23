13c3919d412e8ec27093af4e16a8b3dc4ffc7513
Diego Salas <diesalasmart@gmail.com>
2022-02-22 23:55:12 -0500

    MyTabPannel component

---
+++ src/components/MyTabPanel.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Box, Typography } from '@mui/material'
+
+export default class MyTabPanel extends Component {
+  render() {
+    const { children, value, index, ...other } = this.props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    )
+  }
+}
+
+MyTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
