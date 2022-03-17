a92b03c6d5c8fdc2bba5789e361688191bcde2f1
Diego Salas <diesalasmart@gmail.com>
2022-03-17 12:35:23 -0500

    create modal component - update profile info

---
+++ src/pages/profile/ProfileEditInfo.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { Box, Grid, Typography } from '@mui/material'
+import MyTextField from '../../components/MyTextField'
+import MyButton from '../../components/MyButton'
+import { objIsNull } from '../../utils/utils'
+import UserModel, { validateEditInfo } from '../../services/models/UserModel'
+
+export default class ProfileEditInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newProfile: new UserModel(),
+      errors: {}
+    }
+  }
+
+  componentDidMount() { 
+    this.setState({newProfile: {...this.props.profile}});
+  }
+
+  onChange = (key, isNumeric = false) => (e = {}) => {
+    const { newProfile } = this.state;
+    let val = isNumeric ? parseInt(e.target.value || '0') : e.target.value;
+    let objectUpdated = { ...newProfile };
+
+    const keys = key.split(".");
+    if (keys.length > 1) {
+      objectUpdated[keys[0]][keys[1]] = val;
+    } else {
+      objectUpdated[key] = val;
+    }
+
+    this.setState({newProfile: objectUpdated});
+  }
+
+  onEdit = () => {
+    const { newProfile } = this.state;
+    const errors = validateEditInfo(newProfile);
+    if (objIsNull(errors)) {
+      this.props.onEdit(newProfile);
+    }
+    this.setState({errors: errors});
+  }
+
+  render() {
+    const { errors, newProfile } = this.state;
+    const { btnLoading } = this.props;
+    const { name, lastName, description } = newProfile;
+
+    return (
+      <Box className='create-post'>
+        <Typography className='name' textAlign="left" sx={{ fontSize: 20, fontWeight: 'bold' }}>
+          Edit profile
+        </Typography>
+        <Box className='form' sx={{mt: '1rem'}}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <MyTextField param='name' label='Name' value={name} errors={errors} onChange={this.onChange}/>
+            </Grid>
+            <Grid item xs={6}>
+              <MyTextField param='lastName' label='Last name' value={lastName} errors={errors} onChange={this.onChange}/>
+            </Grid>
+          </Grid>
+          <MyTextField param='description' label='Description' value={description} multiline={true} rows={4} mb={0} onChange={this.onChange}/>
+        </Box>
+        <Box className='jc-r'>
+          <MyButton text='Cancel' variant='secondary' onClick={this.props.onCancel}/>
+          <MyButton text='Post' loading={btnLoading} onClick={this.onEdit}/>
+        </Box>
+      </Box>
+    )
+  }
+}
