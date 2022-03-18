8aa2a3292218a0e59bc9ddce28734a0e7e967b41
Diego Salas <diesalasmart@gmail.com>
2022-03-18 01:34:07 -0500

    include select time option and fetch posts on change

--- src/pages/mostLiked/MostLiked.js
+++ src/pages/mostLiked/MostLiked.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as postActions from '../../services/redux/actions/postActions'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import Wrapper from '../../components/Wrapper'
 import Posts from '../posts/Posts'
+import MySelectField from 'src/components/MySelectField'
 
 class MostLiked extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [],
+      options: {
+        timeOption: 0
+      },
       loading: true
     }
   }
 
-  async componentDidMount() { 
-    const posts = await this.props.getMostLikedPosts();
+  componentDidMount() { 
+    this.fetchPosts();
+  }
+
+  fetchPosts = async () => {
+    this.setState({loading: true});
+    const { options } = this.state;
+    const posts = await this.props.getMostLikedPosts(options.timeOption);
     const postReducer = this.props.post;
     if (postReducer.failed) {
       this.props.showNotification(postReducer.error);
@@ -24,6 +34,23 @@ class MostLiked extends Component {
     } else {
       this.setState({posts, loading: false});
     }
+  }
+
+  onChange = (key, isNumeric = false) => (e = {}) => {
+    const { options } = this.state;
+    let val = isNumeric ? parseInt(e.target.value || '0') : e.target.value;
+    let objectUpdated = { ...options };
+
+    const keys = key.split(".");
+    if (keys.length > 1) {
+      objectUpdated[keys[0]][keys[1]] = val;
+    } else {
+      objectUpdated[key] = val;
+    }
+
+    this.setState({options: objectUpdated}, () => {
+      this.fetchPosts();
+    });
   }
 
   updatePosts = (post) => {
@@ -48,11 +75,17 @@ class MostLiked extends Component {
   }
 
   render() {
-    const { loading, posts } = this.state;
+    const { loading, posts, options } = this.state;
+    const data = [{value: 0, label: 'Today'}, {value: 1, label: 'All week'}, {value: 2, label: 'All month'}];
 
     return (
       <Wrapper loading={loading}>
         <Box className='home'>
+          <Box className='form timeOption'>
+            <div></div>
+            <MySelectField param='timeOption' data={data} value={options.timeOption} onChange={this.onChange}/>
+          </Box>
+
           <Posts {...this.props} posts={posts} updatePosts={this.updatePosts} updateAuthor={this.updateAuthor}/>
         </Box>
       </Wrapper>
