3ab02581ed3ccbd30852481835e387d922bdc17e
Diego Salas <diesalasmart@gmail.com>
2022-03-20 18:39:52 -0500

    validate message model

--- src/services/models/MessageModel.js
+++ src/services/models/MessageModel.js
@@ -2,3 +2,16 @@ export default class MessageModel {
   _id = "";
   message = "";
 }
+
+
+const validate = (message) => {
+  let errors = {
+    message: null,
+  }
+  if (!message.message) {
+    errors.message = "Message can't be empty";
+  }
+  return errors;
+}
+
+export { validate }
